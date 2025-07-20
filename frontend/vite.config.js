e9cf381b2875ffd4f65be8e49f5921dbcf44b968
Joanne <joanne.bazin@gmail.com>
2025-07-20 13:41:14 +0200

    refactor: restructure TS configs

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -51,8 +56,8 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      "@": "/src",
-      "@shared": "../shared/dist",
+      "@": path.resolve(__dirname, "src"),
+      "@shared": path.resolve(__dirname, "../shared/dist"),
     },
   },
 });
