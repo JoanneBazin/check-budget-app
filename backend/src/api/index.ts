0d22fc35f3e44d4f691e7c398ac7b1a678c6eb7b
Joanne <joanne.bazin@gmail.com>
2025-08-10 00:09:22 +0200

    chore: config vercel.json

--- backend/src/api/index.ts
+++ backend/src/api/index.ts
@@ -1,6 +1,10 @@
 import { Request, Response } from "express";
 import app from "../app";
 
+console.log("Démarrage du point d'entrée Vercel...");
+
 export default function handler(req: Request, res: Response) {
+  console.log("Requête reçue sur l'API : ", req.url);
+
   return app(req, res);
 }
