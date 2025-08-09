15d228ded1cabbdcf4ff4d9fd4384c4fbacbed17
Joanne <joanne.bazin@gmail.com>
2025-08-09 21:46:56 +0200

    chore: modify tsupconfig - shared

--- shared/tsup.config.ts
+++ shared/tsup.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: {
-    "schemas/index": "src/schemas/index.ts",
-    "types/index": "src/types/index.ts",
-  },
+  entry: ["src/index.ts", "src/schemas/index.ts", "src/types/index.ts"],
   format: ["cjs", "esm"],
   dts: true,
   clean: true,
+  outDir: "dist",
 });
