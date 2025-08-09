a5893c7b6da695023fceba419ae4564343d9adda
Joanne <joanne.bazin@gmail.com>
2025-08-10 01:05:07 +0200

    chore: config routes proxy (back root)

--- backend/src/api/index.ts
+++ backend/src/api/index.ts
@@ -1,8 +1,6 @@
 import { Request, Response } from "express";
 import app from "../app";
 
-console.log("Démarrage du point d'entrée Vercel...");
-
 export default function handler(req: Request, res: Response) {
   console.log("Requête reçue sur l'API : ", req.url);
 
