3d0db2c6c4dd18c6a6f32d1e8b4232dd5ae3c4e0
Joanne <joanne.bazin@gmail.com>
2025-08-11 16:19:10 +0200

    chore: modify 404 API routes

--- backend/src/api/index.ts
+++ backend/src/api/index.ts
@@ -2,5 +2,8 @@ import { Request, Response } from "express";
 import app from "../app";
 
 export default function handler(req: Request, res: Response) {
+  if (req.url === "/" || req.url === "") {
+    return res.status(404).send("Not Found");
+  }
   return app(req, res);
 }
