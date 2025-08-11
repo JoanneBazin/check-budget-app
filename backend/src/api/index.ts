dd9e2d61e1c56e778468448cd0cd6f684833f743
Joanne <joanne.bazin@gmail.com>
2025-08-11 16:14:40 +0200

    fix: add /api routes 404

--- backend/src/api/index.ts
+++ backend/src/api/index.ts
@@ -2,7 +2,5 @@ import { Request, Response } from "express";
 import app from "../app";
 
 export default function handler(req: Request, res: Response) {
-  console.log("Requête reçue sur l'API : ", req.url);
-
   return app(req, res);
 }
